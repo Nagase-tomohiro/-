f69f35d68c88529e68559ea15ab8f00fb1768a63
Nagase Tomohiro <t_nagase@uniss.co.jp>
2025-12-23 14:07:21 +0900

    文字変更

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const btn = document.getElementById('btn');
 const msg = document.getElementById('msg');
 
 btn.addEventListener('click', () => {
-    msg.textContent = 'こんにちは！ボタンが押されました。';
+    msg.textContent = 'こんにちは！ボタンが押したね？';
     msg.style.color = 'green';
 });
